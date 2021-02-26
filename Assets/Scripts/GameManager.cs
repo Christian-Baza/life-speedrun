e301d7d6bca31e6504fc7173cd29de83c06dbf9d
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-02-26 21:03:08 +0100

    Timern
    Jag jobbade med timern och gameover screenen men det är fortfarande work in progress
    Jag kommer att fortsätta med de imorgon

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public float TimeLeft = 60f;
     public float StartTime = 60f;
+    public string TimeLeft = "60";
+    public TMP_Text TimeLeftUI;
+    float timePassed;
+    public GameObject gameOverUI;
+    public TMP_Text gameOverText;
+
+    private void Start()
+    {
+        timePassed = StartTime + Time.time;
+    }
     private void Update()
     {
-        if (TimeLeft <= 0)
+        if (timePassed <= Time.time)
         {
-            TimeLeft = 0f;
+            TimeLeft = "0s";
+            EndScene(false);
         }
         else
         {
-            TimeLeft = StartTime - Time.time;
+            TimeLeft = (timePassed - Time.time).ToString("F1") + "s";
+            
         }
+        TimeLeftUI.text = TimeLeft;
+    }
+    private void EndScene(bool wonGame)
+    {
+        Time.timeScale = 0f;
+        gameOverUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+
+        gameOverText.text = "You didn't make it in time for the leaderboards";
+    }
+    private void Awake()
+    {
+        Time.timeScale = 1f;
     }
 }
---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Level One");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
