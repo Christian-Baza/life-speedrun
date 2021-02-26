d910a0ee5709861bcf2153f2bbdac9d7914635a0
oscarferm <79453808+oscarferm@users.noreply.github.com>
2021-02-26 16:36:13 +0100

    Oscar(pick uppcode)
    E

---
+++ Assets/targetpickupp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetpickupp : MonoBehaviour
+{
+    public float health = 100f;
+
+    public GameObject cam;
+
+
+    public void Start()
+    {
+
+    }
+
+
+    public void takedamage(float amount)
+    {// det hela skripetet gör är holler hp på objektet den siter på och sedan ifall det är mindte en 0 eller är så destoryar jag det
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+
+            
+        }
+    }
+
+    void Die()
+    {
+
+        
+        Destroy(gameObject);
+
+    }
+
+}
