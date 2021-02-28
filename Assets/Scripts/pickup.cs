4d40c98d0f36f91c2a02c6d37e1f6911b66a767e
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-02-28 19:16:12 +0100

    door

--- Assets/Scripts/pickup.cs
+++ Assets/Scripts/pickup.cs
@@ -60,32 +60,36 @@ public class pickup : MonoBehaviour
         {
             target.animator.SetTrigger("Open door");
         }
-    // skapar en ray cast och sedan kollar ifall targeten har ett script på sig som då sedan gör så vi kan läga till damage på det objektet som då vi kan ta bort det objektet
-                //GameObject impactgo = Instantiate(pickupefect , hit.point, Quaternion.LookRotation(hit.normal));
-                //Destroy(impactgo, 2f);
-        
-            //  GameObject impactgo = Instantiate(hitefect, hit.point, Quaternion.LookRotation(hit.normal));
-            // Destroy(impactgo, 2f);
-            //, range, mask
+        else if (target.ID == 1 && gameManager.itemsAmount[1] >= 1)
+        {
+            target.animator.SetTrigger("Open door");
+        }
+        // skapar en ray cast och sedan kollar ifall targeten har ett script på sig som då sedan gör så vi kan läga till damage på det objektet som då vi kan ta bort det objektet
+        //GameObject impactgo = Instantiate(pickupefect , hit.point, Quaternion.LookRotation(hit.normal));
+        //Destroy(impactgo, 2f);
+
+        //  GameObject impactgo = Instantiate(hitefect, hit.point, Quaternion.LookRotation(hit.normal));
+        // Destroy(impactgo, 2f);
+        //, range, mask
 
 
-            // så vi kan kålla efter specifica object i scenen när vi tar bort något så kan vi ändra en bool som sägjer att vi har den elle inte
+        // så vi kan kålla efter specifica object i scenen när vi tar bort något så kan vi ändra en bool som sägjer att vi har den elle inte
 
-            
 
-             /*
-            if (key.scene.IsValid())
-            {
-                print("no");
-                // go is an instance of an object that's present in the scene
-            }
-            else
-            {
-                print("yes");
-                // go is an instance of a prefab
-            }
-            */
-        
+
+        /*
+       if (key.scene.IsValid())
+       {
+           print("no");
+           // go is an instance of an object that's present in the scene
+       }
+       else
+       {
+           print("yes");
+           // go is an instance of a prefab
+       }
+       */
+
 
 
 
