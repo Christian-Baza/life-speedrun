1cceaa7b9287e9084318978c90c440a4d8f38c67
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-03-01 13:19:56 +0100

    eeee

---
+++ Assets/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            gameManager.EndScene("You somehow fell out of the world", 2);
+        }
+    }
+}
